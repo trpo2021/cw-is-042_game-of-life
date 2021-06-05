2f6e839668b31831df3566ceab7302d549d7b12f
Neytral100 <rymarev.maksim@mail.ru>
2021-06-05 18:01:17 +0700

    Fix menu

--- src/libgameoflife/menu.c
+++ src/libgameoflife/menu.c
@@ -50,31 +50,6 @@ int set_random(Settings* settings)
     return 0;
 }
 
-int setup_galaxy(Settings* settings)
-{
-    settings->width = 20;
-    settings->height = 20;
-    return 2;
-}
-int setup_fish(Settings* settings)
-{
-    settings->width = 100;
-    settings->height = 18;
-    return 3;
-}
-int setup_gun(Settings* settings)
-{
-    settings->width = 50;
-    settings->height = 50;
-    return 4;
-}
-int setup_x(Settings* settings)
-{
-    settings->width = 30;
-    settings->height = 30;
-    return 5;
-}
-
 int change_size_field(Settings* settings)
 {
     system("clear");
